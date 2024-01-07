436a46df5fe42ab08b74c7fae61d3a0aeb6cad4c
Hamed Hasan <hamedhasan422421@gmail.com>
2024-01-07 17:17:49 +0300

    👍 Implemented fetching admin list functionality. 📦 Added getAdminList and fetchAdminList functions. 🛠️ Improved error handling in getAdminList. 🚀 Refactored code for better readability. 🔄 Updated admin list fetching logic.

--- src/services/api.js
+++ src/services/api.js
@@ -3,3 +3,8 @@ import axiosInstance from './axiosInstance';
 export const fetchEmployeeList = () => {
     return axiosInstance.get('/Tasks/getemployeelist');
 };
+
+
+export const fetchAdminList = () => {
+    return axiosInstance.get('/Tasks/getadminlist');
+};
--- src/services/businessLogic.js
+++ src/services/businessLogic.js
@@ -3,11 +3,22 @@ import * as api from './api';
 export const getEmployeeList = async () => {
     try {
         const response = await api.fetchEmployeeList();
-        // You can add more business logic here if needed
+        // Add any additional business logic if needed
         return response.data;
     } catch (error) {
         console.error('Error fetching employee list:', error);
-        // Handle error appropriately
+        throw error;
+    }
+};
+
+
+export const getAdminList = async () => {
+    try {
+        const response = await api.fetchAdminList();
+        // Add any additional business logic if needed
+        return response.data;
+    } catch (error) {
+        console.error('Error fetching admin list:', error);
         throw error;
     }
 };
