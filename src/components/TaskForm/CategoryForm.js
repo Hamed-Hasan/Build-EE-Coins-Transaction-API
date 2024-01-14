46842180c486c2d80d3ccd31ce5a836bd4201289
Hamed Hasan <hamedhasan422421@gmail.com>
2024-01-14 09:51:41 +0300

    🚀 Added loading state during category submission. 📤 Implemented FormData for sending 'Name' and 'Coins' data. 🔧 Handled successful response in onSubmit function. 🚨 Enhanced error handling for category submission failure. ⏳ Added setTimeout to simulate loading state removal.

--- src/components/TaskForm/CategoryForm.js
+++ src/components/TaskForm/CategoryForm.js
@@ -27,18 +27,22 @@ const CategoryForm = ({ defaultValues, onFormFocus }) => {
       const response = await postAddOrEditCategory(formData);
       console.log('Server Response:', response);
 
+      // Call setSuccess only when the request is successful.
+      setSuccess('Category successfully saved!');
       reset();
-
     } catch (error) {
       console.error('Error submitting category:', error);
+
+      // Call setError with the specific error message.
       setError('Failed to save category. Please try again.');
     } finally {
+      // Remove the setSuccess call from here
       setTimeout(() => {
         setLoading(false);
-        setSuccess('Category successfully saved!');
       }, 2000);
     }
   };
+
 
   useEffect(() => {
     if (alert.message && !loading) {
@@ -78,22 +82,24 @@ const CategoryForm = ({ defaultValues, onFormFocus }) => {
 
         </Grid>
         <Grid item xs={12}>
-          <TextField
-            {...register("coins", {
-              required: "Coins are required",
-              valueAsNumber: true,
-              validate: value => {
-                const isValid = /^[1-9]\d*$/.test(value) && !/^0\d+$/.test(value);
-                return isValid || "Coins should be a non-zero integer without leading zeros";
-              }
-            })}
-            label="Coins"
-            variant="outlined"
-            fullWidth
-            type="number"
-            error={!!errors.coins}
-            helperText={errors.coins ? errors.coins.message : ""}
-          />
+        <TextField
+        {...register("coins", {
+          required: "Coins are required",
+          valueAsNumber: true,
+          validate: value => {
+            // This regex allows only positive integers without leading zeros and disallows '0'
+            const isValid = /^[1-9]\d*$/.test(value);
+            return isValid || "Coins should be a positive integer without leading zeros";
+          }
+        })}
+        label="Coins"
+        variant="outlined"
+        fullWidth
+        type="number"
+        error={!!errors.coins}
+        helperText={errors.coins ? errors.coins.message : ""}
+      />
+
 
         </Grid>
         <Grid item xs={12}>
