5231422d545483ced38e77a54915c0d836cd8645
Hamed Hasan <hamedhasan422421@gmail.com>
2024-01-17 17:29:08 +0300

    add files

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import ReusableModal from "@/components/AlertDialogSlide/ReusableModal";
+import AnimationCoinModal from "@/components/AnimationCoinModal/AnimationCoinModal";
 import CoinsButton from "@/components/CoinsButton/CoinsButton";
 
 const HomePage = () => {
   return (
     <div>
+      <AnimationCoinModal/>
       <CoinsButton />
       <ReusableModal />
     </div>
